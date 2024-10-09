4582427ec4966c7f86a4e3d55ffe305dc0a995ec
YuliiaSydorenko <o972986990f@gmail.com>
2024-10-09 08:55:56 +0200

    Update CalcApp.java

--- src/homework19/CalcApp.java
+++ src/homework19/CalcApp.java
@@ -1,23 +1,48 @@
 package homework19;
 
-import homework18.Calculator;
+import static java.lang.Math.*;
 
 public class CalcApp {
+    private static double circumference;
+
     public static void main(String[] args) {
 
 
         double a = 14.5;
         double b = 2.0;
-       double PI = 3.141591;
-       double r =
+        //double pi = PI;
+        double r = 5.0;
 
 
 
-        // Вызов методов класса
+
+
+
+
+        Calculator calc = new Calculator();
+
         System.out.println("Addition: " + Calculator.add(a, b));
         System.out.println("Subtraction: " + Calculator.subtract(a, b));
         System.out.println("Multiplication: " + Calculator.multiply(a, b));
         System.out.println("Division: " + Calculator.divide(a, b));
+
+        // Площадь и длина окружности
+        double radius = 3.0;
+        System.out.println("Площадь круга с радиусом " + radius + ": " + Calculator.circleArea(radius));
+        System.out.println("Длина окружности с радиусом " + radius + ": " + Calculator.circleCircumference(radius));
+
+
+
+      
+
+
+
     }
-}
+    }
+
+
+
+
+
+
 
