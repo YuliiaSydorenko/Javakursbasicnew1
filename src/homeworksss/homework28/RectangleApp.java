0f2222e25825e4680707f48a283c6fd6e89d0906
YuliiaSydorenko <o972986990f@gmail.com>
2024-10-21 13:25:59 +0200

    21.10.2024

--- src/homeworksss/homework28/RectangleApp.java
+++ src/homeworksss/homework28/RectangleApp.java
@@ -4,6 +4,9 @@ import lessonssssss.lesson28.equals.Employee;
 import lists.MyArrayList;
 import lists.MyList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RectangleApp {
     public static void main(String[] args) {
         MyList<Rectangle> rectangleMyList = new MyArrayList<>();
@@ -13,12 +16,31 @@ public class RectangleApp {
 
 
         System.out.println("rectangle == rectangle1: " + rectangle.equals(rectangle1));
-
         System.out.println("rectangle == rectangle2: " + rectangle.equals(rectangle2));
-
         System.out.println("rectangle2 == rectangle1: " + rectangle2.equals(rectangle1));
+
+
+       /* List<Rectangle> rectangleList = new ArrayList<>(List.of(rectangle, rectangle1, rectangle2));
+
+        System.out.println("Список до удаления дубликатов:");
+        rectangleList.forEach(System.out::println);
+        boolean rect = false;
+        System.out.println(rect);
+
+        // Удаляем все прямоугольники, равные rectangle
+        removeMatchingRectangles(rectangleList, rectangle);
+
+        System.out.println("\nСписок после удаления дубликатов:");
+        rectangleList.forEach(System.out::println);
+        System.out.println(rect);
 
 
     }
 
+    private static void removeMatchingRectangles(List<Rectangle> rectangleList, Rectangle rectangle) {
+    }
+*/
+
+    }
 }
+
