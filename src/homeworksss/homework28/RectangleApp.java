7aa5c1ab21f7d26c9f2a015f04627229d367d45a
YuliiaSydorenko <o972986990f@gmail.com>
2024-10-22 09:25:08 +0200

    21.10.2024

--- src/homeworksss/homework28/RectangleApp.java
+++ src/homeworksss/homework28/RectangleApp.java
@@ -1,6 +1,5 @@
 package homeworksss.homework28;
 
-import lessonssssss.lesson28.equals.Employee;
 import lists.MyArrayList;
 import lists.MyList;
 
@@ -15,32 +14,36 @@ public class RectangleApp {
         Rectangle rectangle2 = new Rectangle(4.0, 2.0);
 
 
+
         System.out.println("rectangle == rectangle1: " + rectangle.equals(rectangle1));
         System.out.println("rectangle == rectangle2: " + rectangle.equals(rectangle2));
         System.out.println("rectangle2 == rectangle1: " + rectangle2.equals(rectangle1));
 
+        List<Rectangle> rectangles = new ArrayList<>();
+        rectangles.add(new Rectangle(5.0, 3.0));
+        rectangles.add(new Rectangle(4.0, 2.0));
+        rectangles.add(new Rectangle(5.0, 3.0)); // Дубликат
+        rectangles.add(new Rectangle(6.0, 4.0));
+        rectangles.add(new Rectangle(5.0, 3.0)); // Дубликат
 
-       /* List<Rectangle> rectangleList = new ArrayList<>(List.of(rectangle, rectangle1, rectangle2));
-
+        // Выводим список до удаления дубликатов
         System.out.println("Список до удаления дубликатов:");
-        rectangleList.forEach(System.out::println);
-        boolean rect = false;
-        System.out.println(rect);
+        for (Rectangle rect : rectangles) {
+            System.out.println(rect);
+        }
 
-        // Удаляем все прямоугольники, равные rectangle
-        removeMatchingRectangles(rectangleList, rectangle);
+        // Создаем объект Rectangle, который нужно удалить (все дубликаты)
+        Rectangle rectangleToRemove = new Rectangle(5.0, 3.0);
+        RectangleUtils.removeDuplicates(rectangles, rectangleToRemove);
 
+        // Выводим список после удаления дубликатов
         System.out.println("\nСписок после удаления дубликатов:");
-        rectangleList.forEach(System.out::println);
-        System.out.println(rect);
-
-
-    }
-
-    private static void removeMatchingRectangles(List<Rectangle> rectangleList, Rectangle rectangle) {
-    }
-*/
-
+        for (Rectangle rect : rectangles) {
+            System.out.println(rect);
+        }
     }
 }
+
+
+
 
